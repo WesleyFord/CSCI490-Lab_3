c66b38858e5b4c4588a01bef2870e634e89afae4
WesleyFord <wesley.ford803@gmail.com>
2021-03-11 18:12:55 -0500

    Finished Lab Part 1

--- app/src/main/java/com/example/csci490_lab3/MainActivity.java
+++ app/src/main/java/com/example/csci490_lab3/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.csci490_lab3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String[] classList = {"Portuguese", "English", "Astronomy", "Geology", "Software Engineering", "Mobile App Development"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, classList);
+        Toast shortMessage = Toast.makeText(this, "Short Click!", Toast.LENGTH_SHORT);
+        Toast longMessage = Toast.makeText(this, "Long Click!", Toast.LENGTH_SHORT);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.class_schedule);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                shortMessage.show();
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                longMessage.show();
+                return true;
+            }
+        });
     }
 }
